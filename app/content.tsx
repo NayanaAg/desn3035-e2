738db89ca93821d963781e1af0ed8adae5ee0fe0
NayanaAg <101566994@georgebrown.ca>
2024-09-10 17:24:10 +0000

    installed bootstrap

--- app/content.tsx
+++ app/content.tsx
@@ -1,10 +1,28 @@
 "use client"
 
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
 
 export default function Content() {
 
     return <>
-        <h1>Hello, Next.js!</h1>
+        <div className="container">
+            <h1>Nayana Agrawal Exercise 2</h1>
+            <div className="row">
+                <div className="col-6">
+                    <h2>React Mapping Tool</h2>
+                </div>
+
+                <div className="col-6">
+                    <h2>React Chart Tool</h2>
+                </div>
+
+                <div className="col-12">
+                    <h2>React Player Tool</h2>
+                </div>
+            </div>
+
+        </div>
     </>
 }
