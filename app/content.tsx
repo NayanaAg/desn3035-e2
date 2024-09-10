2764e18ba0fe044b69a8e2149145a3acbdf78837
NayanaAg <101566994@georgebrown.ca>
2024-09-10 17:50:08 +0000

    installed player, charts and added to bootstrap html

--- app/content.tsx
+++ app/content.tsx
@@ -2,7 +2,54 @@
 
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import ReactPlayer from 'react-player';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
+
+const data = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
 
 export default function Content() {
 
@@ -12,14 +59,37 @@ export default function Content() {
             <div className="row">
                 <div className="col-6">
                     <h2>React Mapping Tool</h2>
+                    
                 </div>
 
                 <div className="col-6">
                     <h2>React Chart Tool</h2>
+                    <ResponsiveContainer width="100%" maxHeight={500}>
+                        <AreaChart
+                            width={500}
+                            height={400}
+                            data={data}
+                            margin={{
+                                top: 10,
+                                right: 30,
+                                left: 0,
+                                bottom: 0,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                            <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                            <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
+                        </AreaChart>
+                    </ResponsiveContainer>
                 </div>
 
                 <div className="col-12">
-                    <h2>React Player Tool</h2>
+                    <h2>React Player</h2>
+                    <ReactPlayer width="100%" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
                 </div>
             </div>
 
